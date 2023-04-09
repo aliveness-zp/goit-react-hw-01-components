9618d19d13f18494839e9f787f0afdc824e63bfe
aliveness-zp <81018609+aliveness-zp@users.noreply.github.com>
2023-04-09 14:00:22 +0300

    test 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+
+import data from './Statistics/data';
+import user from './Profile/user';
+console.log(user);
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'column',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title={data.title} stats={data.stts} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,11 @@
+import friends from './friends';
+
+export const FriendList = ({ avatar, name, isOnline }) => {
+  return (
+    <li class="item">
+      <span class="status"></span>
+      <img class="avatar" src="" alt="User avatar" width="48" />
+      <p class="name"></p>
+    </li>
+  );
+};
